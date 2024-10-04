68572e96f41e05f35ebe1ac2b5a617834058b62e
vuthienbao345 <vuthienbao345@gmail.com>
2024-10-03 17:42:22 -0700

    implement hero && features section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 import clsx from 'clsx'
 
 
-const NavLink = ({ title }) => (
-  <LinkScroll
-    className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5'
-  >
-    {title}
-  </LinkScroll>
-)
+
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass='nav-active'
+      className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5'
+    >
+      {title}
+    </LinkScroll>
+  )
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32 )
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
 
   return (
-    <header className='fixed top-0 left-0 z-50 w-full py-10'>
+    <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
       <div className='container flex h-14 items-center max-lg:px-5'>
         <a className='lg:hidden flex-1 cursor-pointer z-2'>
           <img src='/images/xora.svg' width={115} height={55} alt='logo' />
@@ -35,7 +55,7 @@ const Header = () => {
                 <li className='nav-logo'>
                   <LinkScroll
                     to='hero'
-                    offset={-100}
+                    offset={-250}
                     spy smooth 
                     className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}
                   >
